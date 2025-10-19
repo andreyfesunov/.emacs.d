528964e966006b50b47140d7f18947ef94ca6b84
andreyfesunov <fsnv04@gmail.com>
2025-10-19 14:10:54 +0300

    feat(multicursor): add keybinds for toggling evil-mc-mode and undo

--- packages/evil-mc.el
+++ packages/evil-mc.el
@@ -6,3 +6,10 @@
 (evil-define-key 'visual evil-mc-key-map
   "A" #'evil-mc-make-cursor-in-visual-selection-end
   "I" #'evil-mc-make-cursor-in-visual-selection-beg)
+
+(leader-def
+  :keymaps 'normal
+  "m" '(:ignore t :which-key "multicursor")
+  "m <escape>" '(keyboard-escape-quit :which-key t)
+  "m t" '(evil-mc-mode :which-key "toggle multicursor"))
+  "m u" '(evil-mc-undo-all-cursors :which-key "undo all cursors"))
